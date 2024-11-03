4617873ebcdd7fe0c3dfdf97fe10c514186acc82
StarukhDaniil <starukh.danil@gmail.com>
2024-11-03 15:49:57 +0200

    Add log() method and move getCurrentTime() method to private section

--- main.cpp
+++ main.cpp
@@ -10,12 +10,6 @@ public:
 		return logger;
 	}
 
-	std::string getCurrentTime() const {
-		auto now = std::chrono::system_clock::now();
-		auto time_t_now = std::chrono::system_clock::to_time_t(now);
-		return std::format("{:%H:%M:%S}", *std::localtime(&time_t_now));
-	}
-
 };
 
 int main() {
