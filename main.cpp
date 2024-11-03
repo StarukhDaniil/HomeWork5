64d87a43a6416745db903618914100aa6b1f706a
StarukhDaniil <starukh.danil@gmail.com>
2024-11-03 17:01:13 +0200

    Fix error and add log levels

--- main.cpp
+++ main.cpp
@@ -2,15 +2,26 @@
 #include <fstream>
 #include <string>
 #include <chrono>
+#include <ctime>
+#include <sstream>
 #pragma warning(disable: 4996)
+
+enum class LogLevels {
+	info = 0,
+	warning = 1,
+	error = 2,
+};
 
 class Logger {
 private:
 	std::string getCurrentTime() const {
 		auto now = std::chrono::system_clock::now();
 		auto time_t_now = std::chrono::system_clock::to_time_t(now);
-		std::tm buffer;
-		return std::format("{:%H:%M:%S}", *std::localtime(&time_t_now));
+		std::tm* tm_now = std::localtime(&time_t_now);
+
+		std::ostringstream oss;
+		oss << std::put_time(tm_now, "%H:%M:%S");
+		return oss.str();
 	}
 
 	Logger() = default;
@@ -20,10 +31,22 @@ public:
 		return logger;
 	}
 
-	void log(std::string message) const {
+	void log(std::string message, LogLevels logLevel) const {
 		std::ofstream logFile("log.txt", std::ios::app);
 		if (logFile.is_open()) {
-			logFile << getCurrentTime() << " - " << message << std::endl;
+			switch (logLevel) {
+			case LogLevels::info:
+				logFile << "Info: " << getCurrentTime() << " - " << message << std::endl;
+				break;
+			case LogLevels::warning:
+				logFile << "Warning: " << getCurrentTime() << " - " << message << std::endl;
+				break;
+			case LogLevels::error:
+				logFile << "Error: " << getCurrentTime() << " - " << message << std::endl;
+				break;
+			default:
+				std::cerr << "Wrong LogLevel catched :(" << std::endl;
+			}
 		}
 		else {
 			std::cerr << "File opening is unsuccesful :(" << std::endl;
@@ -36,9 +59,7 @@ public:
 };
 
 int main() {
-	int a;
-	Logger::getInstance().log("asd");
-	std::cin >> a;
-	Logger::getInstance().log("asdfg");
+	Logger::getInstance().log("asd", LogLevels::info);
+	Logger::getInstance().log("asd", LogLevels::warning);
 	return 0;
 }
