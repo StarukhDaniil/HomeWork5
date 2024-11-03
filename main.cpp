996e51f555125d23ed361a2c5e3cd27a5d6e0bb4
StarukhDaniil <starukh.danil@gmail.com>
2024-11-03 15:41:12 +0200

    Add getCurrentTime() method

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <chrono>
 
 class Logger {
 public:
@@ -8,6 +9,13 @@ public:
 		static Logger logger;
 		return logger;
 	}
+
+	std::string getCurrentTime() const {
+		auto now = std::chrono::system_clock::now();
+		auto time_t_now = std::chrono::system_clock::to_time_t(now);
+		return std::format("{:%H:%M:%S}", *std::localtime(&time_t_now));
+	}
+
 };
 
 int main() {
